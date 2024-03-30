8e1ad248f1ec385e63081fdc8552cdf73b06bde5
lesan <lentrix12345@gmail.com>
2024-03-29 23:27:17 -0500

    perdon gatito

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <p className="text-3xl">Abraham baboso</p>
+      <p className="text-3xl">Perdoname porfavor</p>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3FssZPKLlY4Zx5ST8DKX2vBLB_GxnTK6M2jn9arLFZ4ybZQ5m9IPo38xpSqvpn3dZ8EM"
         alt="Garaynismo"
